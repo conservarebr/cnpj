4a80b0b72e6d45e29e682649f93634044e585d71
Ribeiro, Francisco <francisco.ribeiro@aecom.com>
2024-10-03 16:52:25 -0300

    Ajustes

--- testes.py
+++ testes.py
@@ -1,26 +1,18 @@
 
 import duckdb
+from app.core.config import settings
 import os
-from app.core.config import Settings
 
-settings = Settings()
-
-def execute_sql_file(file_path, data_path):
+def execute_sql_file(file_path):
     with open(file_path, 'r') as file:
-       
-        sql_commands = file.read().replace('${data_path}', data_path).split(';')
+        sql_script = file.read()
+        
+    conn = duckdb.connect(database=':memory:')
+    conn.execute(sql_script)
+    conn.commit()
+    conn.close()
 
-    conn = duckdb.connect(database=':memory:') 
-
-    for command in sql_commands:
-        command = command.strip()
-        if command: 
-            try:
-                conn.execute(command)
-            except Exception as e:
-                print(f"Erro ao executar o comando: {command}")
-                print(e)
-
-sql_file_path = os.path.join('/home/fribeiro/src/cnpj/app/scripts', 'CNPJ.sql')
-
-execute_sql_file(sql_file_path, settings.data_path)
+if __name__ == "__main__":
+    sql_file_path = os.path.join('/home/fribeiro/src/cnpj/app/scripts', 'CNPJ.sql')
+    execute_sql_file(sql_file_path)
+    print("SQL script executed successfully.")
