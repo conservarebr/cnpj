95b94f34f9f82c78d52ec3e5a606a14c14019776
Ribeiro, Francisco <francisco.ribeiro@aecom.com>
2024-10-03 14:59:28 -0300

    Ajustes

--- testes.py
+++ testes.py
@@ -1,30 +1,30 @@
-import os
-from app.core.config import Settings
-from app.sco import settings
+#import os
+#from app.core.config import Settings
+#from app.sco import settings
 
 # Carregar as configurações
-settings = Settings()
+#settings = Settings()
 
 # Listar os arquivos CSV que você espera encontrar
-csv_files = [
-    "cnaes.csv",
-    "municipios.csv",
-    "estabelecimentos0.csv",
-    "estabelecimentos1.csv",
-    "estabelecimentos2.csv",
-    "estabelecimentos3.csv",
-    "estabelecimentos4.csv",
-    "estabelecimentos5.csv",
-    "estabelecimentos6.csv",
-    "estabelecimentos7.csv",
-    "estabelecimentos8.csv",
-    "estabelecimentos9.csv",
-]
+#csv_files = [
+    #"cnaes.csv",
+    #"municipios.csv",
+    #"estabelecimentos0.csv",
+    #"estabelecimentos1.csv",
+    #"estabelecimentos2.csv",
+    #"estabelecimentos3.csv",
+    #"estabelecimentos4.csv",
+    #"estabelecimentos5.csv",
+   # "estabelecimentos6.csv",
+   # "estabelecimentos7.csv",
+   # "estabelecimentos8.csv",
+   # "estabelecimentos9.csv",
+#]
 
 # Verificar se cada arquivo existe
-for csv_file in csv_files:
-    file_path = os.path.join(settings.data_path, csv_file)
-    if os.path.exists(file_path):
-        print(f"Arquivo encontrado: {file_path}")
-    else:
-        print(f"Arquivo não encontrado: {file_path}")
+#for csv_file in csv_files:
+    #file_path = os.path.join(settings.data_path, csv_file)
+    #if os.path.exists(file_path):
+       # print(f"Arquivo encontrado: {file_path}")
+    #else:
+        #print(f"Arquivo não encontrado: {file_path}")
