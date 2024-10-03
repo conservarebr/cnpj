9593a411ff094a388f9bd68214104e3bc9c7db53
Ribeiro, Francisco <francisco.ribeiro@aecom.com>
2024-10-03 15:20:49 -0300

    Ajustes

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,8 @@
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
+import os
 
+load_dotenv("/home/fribeiro/src/cnpj/example.env")
 
 class Settings(BaseSettings):
     data_path: str
