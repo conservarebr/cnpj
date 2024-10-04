ee7f7990ee6aa8145ef9576c7982976dff0e98b4
Ribeiro, Francisco <francisco.ribeiro@aecom.com>
2024-10-04 13:34:14 -0300

    Ajustes

---
+++ app/__ini__.py
@@ -0,0 +1,12 @@
+
+from dotenv import load_dotenv
+from app.core.config import Settings
+import os
+
+# Carregar o arquivo .env
+load_dotenv("/home/fribeiro/src/cnpj/example.env")
+
+# Carregar as configurações
+settings = Settings()
+
+
