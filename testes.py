300d146d0ca36df7a8572a4e47693ddf93414e63
Ribeiro, Francisco <francisco.ribeiro@aecom.com>
2024-10-07 12:49:03 -0300

    Ajustes

--- testes.py
+++ testes.py
@@ -6,6 +6,7 @@ data_fribeiro = "/home/fribeiro"
 conn = duckdb.connect(database=':memory:')
 
 #### Municipios ####
+
 conn.execute("""
 CREATE TABLE municipios (
     codigo VARCHAR PRIMARY KEY,
@@ -22,11 +23,12 @@ COPY municipios FROM '{municipios_file_path}'
 result = conn.execute("SELECT * FROM municipios LIMIT 10").fetchall()
 for row in result:
     print(row)
-
+    
 #### Estabelecimentos ####
+
 estabelecimentos_files = [os.path.join(data_path, f'estabelecimentos{i}.csv') for i in range(10)]
 
-conn.execute(f"""
+conn.execute("""
 CREATE TABLE estabelecimentos AS 
 SELECT 
     column00 AS cnpj_basico,
@@ -34,7 +36,7 @@ SELECT
     column02 AS cnpj_dv,
     column05 AS situacao_cadastral,
     column11 AS cnae_fiscal_principal,
-    TRIM(split.value) AS cnae_secundaria,
+    column12 AS cnae_fiscal_secundaria,
     column13 AS tipo_de_logradouro,
     column14 AS logradouro,
     column15 AS numero,
@@ -42,7 +44,7 @@ SELECT
     column17 AS bairro,
     column18 AS cep,
     column19 AS uf,
-    column19 AS municipio
+    column20 AS municipio
 FROM read_csv_auto(
     ['{0}'],
     sep = ';',
@@ -50,19 +52,15 @@ FROM read_csv_auto(
     ignore_errors = true,
     union_by_name = true,
     filename = true
-) AS subquery
-CROSS JOIN UNNEST(string_split(column12, ',')) AS split(value);
+) AS subquery;
 """.format("', '".join(estabelecimentos_files)))
 
 result = conn.execute("SELECT * FROM estabelecimentos LIMIT 10").fetchall()
 for row in result:
     print(row)
-
-result = conn.execute("SELECT count(*) FROM estabelecimentos").fetchall()
-for row in result:
-    print(row)
-
+    
 #### Cnae ####
+
 conn.execute("""
 CREATE TABLE cnae (
     codigo VARCHAR PRIMARY KEY,
@@ -80,4 +78,90 @@ result = conn.execute("SELECT * FROM cnae LIMIT 10").fetchall()
 for row in result:
     print(row)
     
+#### Ajuste 1: Realizando o split em Estabelecimentos ####
+
+conn.execute("""
+CREATE TABLE temp_estabelecimentos AS
+SELECT 
+    cnpj_basico,
+    cnpj_ordem,
+    cnpj_dv,
+    situacao_cadastral,
+    cnae_fiscal_principal,
+    TRIM(split.value) AS cnae_secundaria,
+    tipo_de_logradouro,
+    logradouro,
+    numero,
+    complemento,
+    bairro,
+    cep,
+    uf, 
+    municipio
+FROM estabelecimentos
+CROSS JOIN UNNEST(string_split(cnae_fiscal_secundaria, ',')) AS split(value);
+""")
+
+conn.execute("DROP TABLE estabelecimentos;")
+
+conn.execute("ALTER TABLE temp_estabelecimentos RENAME TO estabelecimentos;")
+
+#### Ajuste 2: Realizando o Join Estabelecimentos x Municipios ####
+    
+conn.execute(""" 
+ALTER TABLE estabelecimentos ADD COLUMN municipio_descricao VARCHAR; 
+""")
+
+conn.execute(""" 
+UPDATE estabelecimentos e 
+SET municipio_descricao = m.descricao 
+FROM municipios m 
+WHERE e.municipio = m.codigo; 
+""")
+
+result = conn.execute("SELECT * FROM estabelecimentos LIMIT 10").fetchall()
+for row in result:
+    print(row)
+
+result = conn.execute("SELECT count(*) FROM estabelecimentos").fetchall()
+for row in result:
+    print(row)
+
+#### Ajuste 3: Filtrando os Cnaes desejados ####
+
+cnae_filtro = [
+    '4110700', '6435201', '6470101', '6470103', 
+    '6810201', '6810202', '6810203', '6821801', 
+    '6821802', '6822600', '7490104'
+]
+
+cnae_filtro_str = "', '".join(cnae_filtro)
+
+conn.execute(f"""
+CREATE TABLE csv AS
+SELECT *
+FROM estabelecimentos
+WHERE cnae_fiscal_principal IN ('{cnae_filtro_str}')
+   OR cnae_secundaria IN ('{cnae_filtro_str}');
+""")
+
+result = conn.execute("SELECT * FROM csv LIMIT 10").fetchall()
+for row in result:
+    print(row)
+
+result = conn.execute("SELECT count(*) FROM csv").fetchall()
+for row in result:
+    print(row)
+    
+#### Salvando em csv ####
+    
+output_file_path = os.path.join('C:/Users/RibeiroF/Downloads/', 'filtered_estabelecimentos.csv')
+
+conn.execute(f"""
+COPY csv TO '{output_file_path}' 
+    (FORMAT CSV, DELIMITER ';', HEADER TRUE, QUOTE '"', ESCAPE '"', ENCODING 'UTF8');
+""")
+
+print(f"Tabela salva em {output_file_path}")
+    
 conn.close()
+
