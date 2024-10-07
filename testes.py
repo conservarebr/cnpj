540afa58061c70927d103305a1c539e01159becd
Ribeiro, Francisco <francisco.ribeiro@aecom.com>
2024-10-07 16:35:50 -0300

    Ajustes

--- testes.py
+++ testes.py
@@ -5,7 +5,15 @@ data_path = "/mnt/disk1/data/cnpj"
 data_fribeiro = "/home/fribeiro"
 conn = duckdb.connect(database=':memory:')
 
-#### Cnae ####
+#### Filtro de CNAEs ####
+cnae_filtro = [
+    '4110700', '6435201', '6470101', '6470103', 
+    '6810201', '6810202', '6810203', '6821801', 
+    '6821802', '6822600', '7490104'
+]
+cnae_filtro_str = "', '".join(cnae_filtro)
+
+#### CNAE ####
 conn.execute("""
 CREATE TABLE cnae (
     codigo VARCHAR PRIMARY KEY,
@@ -33,16 +41,17 @@ COPY municipios FROM '{municipios_file_path}'
 
 #### Estabelecimentos ####
 estabelecimentos_files = [os.path.join(data_path, f'estabelecimentos{i}.csv') for i in range(10)]
+estabelecimentos_files_str = ', '.join([f"'{file}'" for file in estabelecimentos_files])
 
 conn.execute(f"""
 CREATE TABLE csv AS
 SELECT 
-    CONCAT(column00, column01, column02) AS cnpj_completo,
-    CONCAT(tipo_de_logradouro, ' ', logradouro, ' ', numero, ' ', bairro, ' ', m.descricao, ' ', uf, ' ', cep) AS endereco,
-    CONCAT(tipo_de_logradouro, ' ', logradouro, ' ', numero, ' ', bairro, ' ', m.descricao, ' ', uf) AS endereco_editado,
-    cep
+    CONCAT(e.column00, e.column01, e.column02) AS cnpj_completo,
+    CONCAT(e.column13, ' ', e.column14, ' ', e.column15, ' ', e.column17, ' ', m.descricao, ' ', e.column19, ' ', e.column18) AS endereco,
+    CONCAT(e.column13, ' ', e.column14, ' ', e.column15, ' ', e.column17, ' ', m.descricao, ' ', e.column19) AS endereco_editado,
+    e.column18 AS cep
 FROM read_csv_auto(
-    ['{0}'],
+    [{estabelecimentos_files_str}],
     sep = ';',
     header = false,
     ignore_errors = true,
@@ -50,20 +59,12 @@ FROM read_csv_auto(
     filename = true
 ) AS e
 JOIN municipios m ON e.column20 = m.codigo
-CROSS JOIN UNNEST(string_split(e.column12, ',')) AS cnae_secundaria
+CROSS JOIN UNNEST(string_split(e.column12, ',')) AS cnae_secundaria(value)  -- Altera para dar um alias
 WHERE e.column05 = '02' AND (
     e.column11 IN ('{cnae_filtro_str}') OR 
-    TRIM(cnae_secundaria) IN ('{cnae_filtro_str}')
+    TRIM(value) IN ('{cnae_filtro_str}')  -- Usa o alias aqui
 );
-""".format("', '".join(estabelecimentos_files), "'".join(cnae_filtro)))
-
-result = conn.execute("SELECT * FROM csv LIMIT 10").fetchall()
-for row in result:
-    print(row)
-
-result = conn.execute("SELECT count(*) FROM csv").fetchall()
-for row in result:
-    print(row)
+""")
 
 #### Salvando em csv ####
 saida = os.path.join(data_fribeiro, 'Teste.csv')
@@ -74,3 +75,13 @@ COPY csv TO '{saida}'
 
 print(f"A tabela 'csv' foi salva em {saida}")
 conn.close()
+
+
+
+#result = conn.execute("SELECT * FROM csv LIMIT 10").fetchall()
+#for row in result:
+    #print(row)
+
+#result = conn.execute("SELECT count(*) FROM csv").fetchall()
+#for row in result:
+    #print(row)
