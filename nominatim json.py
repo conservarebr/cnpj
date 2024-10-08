3aa6ec024c088dc66166427b14af72b04e835ef1
Ribeiro, Francisco <francisco.ribeiro@aecom.com>
2024-10-08 16:05:12 -0300

    Ajustes

--- nominatim json.py
+++ nominatim json.py
@@ -1,4 +1,3 @@
-
 import duckdb
 import os
 import pandas as pd
@@ -30,7 +29,7 @@ def geocode_addresses(file_path, num_rows=None):
     data_fribeiro = os.path.dirname(file_path)
     conn = duckdb.connect(database=':memory:')
     
-    df = pd.read_csv(file_path, sep=';', encoding='UTF-8')
+    df = pd.read_csv(file_path, sep=';', encoding='utf-8')
     
     if num_rows is not None:
         df = df.head(num_rows)
@@ -40,15 +39,52 @@ def geocode_addresses(file_path, num_rows=None):
     for endereco in df['endereco_editado']:
         result = geocode_address(endereco)
         results.append(result)
-        time.sleep(1)  
+        time.sleep(1)  # Aguarda 1 segundo entre as requisições
 
     df['resultado_geocodificacao'] = [json.dumps(result, ensure_ascii=False) for result in results]
 
     saida_geocodificado = os.path.join(data_fribeiro, 'Teste_geocodificado.csv')
-    df.to_csv(saida_geocodificado, sep=';', index=False, encoding='ISO-8859-1')
+    df.to_csv(saida_geocodificado, sep=';', index=False, encoding='utf-8')
 
     print(f"O arquivo geocodificado foi salvo em {saida_geocodificado}")
     conn.close()
-
-geocode_addresses("/home/fribeiro/Teste.csv", num_rows=100)  # Para geocodificar um número específico de linhas
+    
+geocode_addresses("/home/fribeiro/Teste.csv", num_rows=400)  # Para geocodificar um número específico de linhas
 # geocode_addresses("/home/fribeiro/Teste.csv")  # Para geocodificar todo o arquivo
+
+
+
+
+
+import asyncio
+from aiohttp import ClientSession
+
+async def fetch(url, session):
+    async with session.get(url) as response:
+        delay = response.headers.get("DELAY")
+        date = response.headers.get("DATE")
+        print("{}:{} with delay {}".format(date, response.url, delay))
+        return await response.text()
+
+async def bound_fetch(sem, url, session):
+    async with sem:
+        await fetch(url, session)
+
+async def run(r):
+    tasks = []
+    sem = asyncio.Semaphore(1000)
+    
+    async with ClientSession() as session:
+        for i in (r):
+            task = asyncio.ensure_future(bound_fetch(sem, i, session))
+            tasks.append(task)
+
+        responses = asyncio.gather(*tasks)
+        await responses
+
+number = 10000
+asyncio.run(run(number))
+
+geocode_addresses("/home/fribeiro/Teste.csv", num_rows=400) 
+
+nessa planilha teste.csv tenho os campos cnpj_concatenado, endereco, endereco_editado e cep, vou utiçizar cnpj_concatenado e para georeferenciar vou utilizar o endereco_editado no nominatim utilize  o codigo acima para fazer isso
