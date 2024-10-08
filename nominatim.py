d317baf76461580de1d7227c5b2cd2b970166832
Ribeiro, Francisco <francisco.ribeiro@aecom.com>
2024-10-08 17:45:21 -0300

    Ajustes

--- nominatim.py
+++ nominatim.py
@@ -1,4 +1,3 @@
-
 import os
 import pandas as pd
 import requests
@@ -6,7 +5,7 @@ import json
 import logging
 from concurrent.futures import ThreadPoolExecutor
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def geocode_address(endereco):
     try:
@@ -18,13 +17,13 @@ def geocode_address(endereco):
         return None 
 
 def geocode_addresses(caminho_arquivo, num_linhas=None):
+    logging.info(f"Iniciando a geocodificação de endereços a partir do arquivo: {caminho_arquivo}")
     df = pd.read_csv(caminho_arquivo, sep=';', encoding='utf-8').head(num_linhas)
     with ThreadPoolExecutor() as executor:
         df['resultado_geocodificacao'] = list(executor.map(geocode_address, df['endereco_editado']))
-    
     df['resultado_geocodificacao'] = df['resultado_geocodificacao'].apply(lambda x: json.dumps(x, ensure_ascii=False) if x else None)
-    df.to_csv(os.path.join(os.path.dirname(caminho_arquivo), 'Teste_geocodificado.csv'), sep=';', index=False, encoding='utf-8')
-    
-    logging.info(f"O arquivo geocodificado foi salvo em {caminho_arquivo}")
+    output_file = os.path.join(os.path.dirname(caminho_arquivo), 'Teste_geocodificado.csv')
+    df.to_csv(output_file, sep=';', index=False, encoding='utf-8')
+    logging.info(f"O arquivo geocodificado foi salvo em {output_file}")
+geocode_addresses("/home/fribeiro/Teste.csv", num_linhas=None)
 
-geocode_addresses("/home/fribeiro/Teste.csv", num_linhas=100000)
