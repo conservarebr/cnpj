c2da0d889f73effc8dab19bf275afa354f7d1b3a
Ribeiro, Francisco <francisco.ribeiro@aecom.com>
2024-10-09 10:51:40 -0300

    Ajustes

--- colecao.py
+++ colecao.py
@@ -1,11 +1,11 @@
 import csv
 
-colecoes = []
+colecao = []
 file_path = '/home/fribeiro/Teste.csv'
 
 with open(file_path, mode='r') as file:
     leitor = csv.DictReader(file, delimiter=';') 
     for linha in leitor:
-         colecoes.append(linha['colecao'])
+         colecao.append(linha['colecao'])
          
-print(colecoes)
+print(colecao)
