581deb6631546c32b7414374fc6000facafd7ce5
Ribeiro, Francisco <francisco.ribeiro@aecom.com>
2024-10-09 16:09:17 -0300

    Ajustes

--- teste.py
+++ teste.py
@@ -7,6 +7,18 @@ from concurrent.futures import ThreadPoolExecutor
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+def get_data_fribeiro():
+    """Retorna o caminho para o diretório de dados."""
+    return "/home/fribeiro"
+
+def get_cnae_filtro():
+    """Retorna a lista de filtros de CNAE."""
+    return [
+        '4110700', '6435201', '6470101', '6470103', 
+        '6810201', '6810202', '6810203', '6821801', 
+        '6821802', '6822600', '7490104'
+    ]
+
 def geocode_address(endereco):
     try:
         response = requests.get(f"http://venus.iocasta.com.br:8080/search.php?q={endereco}")
@@ -22,10 +34,18 @@ def geocode_addresses(caminho_arquivo, num_linhas=None):
     
     with ThreadPoolExecutor() as executor:
         df['resultado_geocodificacao'] = list(executor.map(geocode_address, df['endereco_editado']))
-    df['resultado_geocodificacao'] = df['resultado_geocodificacao'].apply(lambda x: json.dumps(x, ensure_ascii=False) if x else None)
+        df['resultado_geocodificacao'] = df['resultado_geocodificacao'].apply(lambda x: json.dumps(x, ensure_ascii=False) if x else None)
     
     output_file = os.path.join(os.path.dirname(caminho_arquivo), 'Teste_geocodificado.csv')
     df.to_csv(output_file, sep=';', index=False, encoding='utf-8')
     logging.info(f"O arquivo geocodificado foi salvo em {output_file}")
+
+def main():
+    data_fribeiro = get_data_fribeiro()
+    cnae_filtro = get_cnae_filtro()
+    cnae_filtro_str = "', '".join(cnae_filtro)
     
-geocode_addresses("/home/fribeiro/Teste.csv", num_linhas=None)
+    geocode_addresses(os.path.join(data_fribeiro, 'Teste.csv'), num_linhas=10)
+
+if __name__ == "__main__":
+    main()
