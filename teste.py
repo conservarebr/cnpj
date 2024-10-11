914c7e3e1ba12ead2c4a0878760b647841d09b4d
Ribeiro, Francisco <francisco.ribeiro@aecom.com>
2024-10-11 15:54:36 -0300

    Ajustes

--- teste.py
+++ teste.py
@@ -16,8 +16,8 @@ def geocode_address(endereco):
         logging.error(f"Erro ao geocodificar o endereço: {endereco}. Erro: {e}")
         return None 
 
-def cnae_filtro(df, cnaes_desejados):
-    return df[df['cnae_primaria'].isin(cnaes_desejados) | df['cnae_secundaria'].isin(cnaes_desejados)]
+def cnae_filtro(df, cnaes):
+    return df[df['cnae_primaria'].isin(cnaes) | df['cnae_secundaria'].isin(cnaes)]
 
 def geocode_addresses(caminho_arquivo, cnaes_desejados, num_linhas=None):
     logging.info(f"Iniciando a geocodificação a partir do arquivo: {caminho_arquivo}")
@@ -25,19 +25,22 @@ def geocode_addresses(caminho_arquivo, cnaes_desejados, num_linhas=None):
     df_filtrado = cnae_filtro(df, cnaes_desejados).drop(columns=['cnae_primaria', 'cnae_secundaria'], errors='ignore').drop_duplicates()
     if num_linhas: df_filtrado = df_filtrado.head(num_linhas)
 
-    logging.info(f"Número de registros restantes: {df_filtrado.shape[0]}")   
-
-    with ThreadPoolExecutor() as executor:
-        for column in ['endereco_editado', 'cep']:
-            df_filtrado[f'resultado_geocodificacao_{column}'] = list(executor.map(geocode_address, df_filtrado[column]))
+    logging.info(f"Número de registros restantes: {df_filtrado.shape[0]}")
     
-    for column in ['resultado_geocodificacao_endereco_editado', 'resultado_geocodificacao_cep']:
-        df_filtrado[column] = df_filtrado[column].apply(lambda x: json.dumps(x, ensure_ascii=False) if x else None)
+    for column in ['endereco_editado', 'cep']:
+        logging.info(f"Iniciando a verificação por {column}")
+        with ThreadPoolExecutor() as executor:
+            df_filtrado[f'resultado_geocodificacao_{column}'] = list(executor.map(geocode_address, df_filtrado[column]))
+        logging.info(f"Concluída verificação por {column}")
 
-    output_file = os.path.join(os.path.dirname(caminho_arquivo), 'Teste_geocodificado_03.csv')
+    df_filtrado[[f'resultado_geocodificacao_{col}' for col in ['endereco_editado', 'cep']]] = df_filtrado[[f'resultado_geocodificacao_{col}' for col in ['endereco_editado', 'cep']]].apply(lambda x: x.apply(lambda y: json.dumps(y, ensure_ascii=False) if y else None))
+
+    output_file = os.path.join(os.path.dirname(caminho_arquivo), 'Teste_geocodificado_04.csv')
     df_filtrado.to_csv(output_file, sep=';', index=False, encoding='utf-8')
     logging.info(f"Arquivo geocodificado salvo em {output_file}")
 
 caminho_arquivo = "/home/fribeiro/bases/Teste_02.csv"
 cnaes_desejados = ['4110700', '6435201', '6470101', '6470103', '6810201', '6810202', '6810203', '6821801', '6821802', '6822600', '7490104']
 geocode_addresses(caminho_arquivo, cnaes_desejados)
+
+# scp fribeiro@209.126.127.15:/home/fribeiro/bases/Teste_geocodificado_03.csv C:/Users/RibeiroF/Downloads/
